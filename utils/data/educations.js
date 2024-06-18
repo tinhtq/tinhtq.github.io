0d6f4f5df0ed0d566ab2a647095dd22207d5923a
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-06-18 23:33:32 +0700

    update information

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,6 +1,7 @@
 export const educations = [
   {
     id: 1,
+    icon: "./hcmut_logo.png",
     title: "Bachelor Degree",
     duration: "2015 - 2019",
     institution: "Ho Chi Minh University of Technology",
