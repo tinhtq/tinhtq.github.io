eb53fa7c460d8da61a1d4fba9d52aa586a5737c7
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-06-19 23:07:01 +0700

    update cert

--- utils/data/certificates.js
+++ utils/data/certificates.js
@@ -9,4 +9,14 @@ export const certificatesData = [
     image: "/png/certificates/aws-sysops.png",
     link: "https://www.credly.com/badges/3f627842-6ce8-4718-a06d-93d4bb35132a/public_url",
   },
+  {
+    name: "Kong Gateway Foundations",
+    image: "/png/certificates/konggw-foundation.png",
+    link: "https://www.credly.com/badges/3c77fefb-e1b6-4f48-a7ee-9f0aa28de510/public_url",
+  },
+  {
+    name: "Hyperledger Blockchain (LFS171)",
+    image: "/png/certificates/hyperledger-blockchain-foundation.png",
+    link: "https://www.credly.com/badges/68ce6478-ce8e-4785-9251-efab935fec3b/public_url",
+  },
 ];
