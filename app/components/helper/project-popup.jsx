12d175e83a6e1d9684974312dd61adc9e980b9ec
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-06-29 22:31:55 +0700

    update global var

--- app/components/helper/project-popup.jsx
+++ app/components/helper/project-popup.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 
 const ProjectPopup = ({ title, image, togglePopup, isOpen }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    // Clean up function
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50 ${
         isOpen ? "" : "hidden"
       }`}
+      style={{ zIndex: 50 }}
     >
       <div className="bg-[#0D1324] p-6 rounded-lg max-w-4xl">
         <div className="flex justify-between items-center">
