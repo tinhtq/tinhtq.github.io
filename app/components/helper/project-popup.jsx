5b1c3bf535c15c9d2ad3d88f234cf43701dbf10e
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-07-05 23:02:51 +0700

    update zindex

--- app/components/helper/project-popup.jsx
+++ app/components/helper/project-popup.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 function ProjectPopup({ title, images, togglePopup, isOpen, settings }) {
   return (
     <div
-      className={`fixed inset-0 bg-black bg-opacity-50 z-10 ${
+      className={`fixed inset-0 bg-black bg-opacity-50 z--10 ${
         isOpen ? "flex" : "hidden"
       } items-center justify-center`}
     >
