da29546a45c2df7345120fe2fe2ca963feb55008
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-07-22 22:55:16 +0700

    update navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -75,6 +75,16 @@ function Navbar() {
               </div>
             </Link>
           </li>
+          <li>
+            <Link
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="/#certificates"
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                CERTIFICATES
+              </div>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
